ef9ce9274031513cf965860a482006c0d1c533cd
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-07 16:19:23 -0700

    Update Header.js

--- facebook-clone/components/Header.js
+++ facebook-clone/components/Header.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import HeaderIcon from './HeaderIcon.js';
+
 import {
     BellIcon,
     ChatIcon,
@@ -23,7 +24,7 @@ export default function Header() {
                 <Image src='https://links.papareact.com/5me' width={40} height={40} layout='fixed'/>
                 <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                     <SearchIcon className='h-6 text-gray-600'/>
-                    <input className='flex flex-shrink ml-2 items-center bg-transparent outline-none' type='text' placeholder='Search Facebook'/>
+                    <input className='flex-shrink ml-2 items-center bg-transparent outline-none hidden md:inline-flex' type='text' placeholder='Search Facebook'/>
                 </div>
             </div>
 
@@ -39,6 +40,13 @@ export default function Header() {
             </div>
 
             {/* Right */}
+            <div className='flex items-center sm:space-x-2 justify-end'>
+                <p className='font-semibold pr-3 whitespace-nowrap'>Jack Brown</p>
+                <ViewGridIcon className='icon' />
+                <ChatIcon className='icon' />
+                <BellIcon className='icon' />
+                <ChevronDownIcon className='icon' />
+            </div>
         </div>
     )
 }
