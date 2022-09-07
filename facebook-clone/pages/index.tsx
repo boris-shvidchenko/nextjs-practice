09c369bb47135a49705396f58638aa9894771a57
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-07 16:21:20 -0700

    Update index.tsx

--- facebook-clone/pages/index.tsx
+++ facebook-clone/pages/index.tsx
@@ -1,19 +1,32 @@
+import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
+import Login from '../components/Login'
+import Sidebar from '../components/Sidebar';
+import Feed from '../components/Feed';
 
 export default function Home() {
+  
+  // state for login
+  const [loginPage, setLoginPage] = React.useState(false)
+
   return (
     <div>
-      <Head>
-        <title>Facebook</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <main>
-        {/* sidebar */}
-        {/* feed */}
-        {/* widgets */}
-      </main>
+      {!loginPage && <Login setLoginPage={setLoginPage}/>}
+      {loginPage &&
+        <div> 
+          <Head>
+            <title>Facebook</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head> 
+          <Header />
+          <main className='flex'>
+            <Sidebar />
+            <Feed />
+            {/* widgets */}
+          </main>
+        </div>
+      }
     </div>
   )
 }
