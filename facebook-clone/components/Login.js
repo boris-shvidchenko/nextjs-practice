c907d985ea29154ac12489d4a97652f5965cbfc3
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-08 16:30:40 -0700

    Update Login.js

--- facebook-clone/components/Login.js
+++ facebook-clone/components/Login.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Login(props) {
 
@@ -8,9 +9,15 @@ export default function Login(props) {
     }
 
     return(
-        <div className='grid place-items-center pt-20'>
-            <Image src='https://links.papareact.com/5me' width={250} height={250} objectFit='contain'/>
-            <h1 onClick={updatePage} className='p-5 mt-10 bg-blue-500 hover:bg-blue-600 rounded-full text-white text-center cursor-pointer'>Login with Facebook</h1>
+        <div>
+            <Head>
+                <title>Facebook</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className='grid place-items-center pt-20'>
+                <Image src='https://links.papareact.com/5me' width={250} height={250} objectFit='contain'/>
+                <h1 onClick={updatePage} className='p-5 mt-10 bg-blue-500 hover:bg-blue-600 rounded-full text-white text-center cursor-pointer'>Login with Facebook</h1>
+            </div>
         </div>
     )
 }
