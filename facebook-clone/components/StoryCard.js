c73ef36e93be09b27b4789b79f001af7694b3bb7
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-08 16:32:58 -0700

    Update StoryCard.js

--- facebook-clone/components/StoryCard.js
+++ facebook-clone/components/StoryCard.js
@@ -4,7 +4,7 @@ export default function StoryCard({ name, src, profile }) {
     return(
         <div className='relative h-14 w-14 md:h-20 md:w-20 lg:h-56 lg:w-32 cursor-pointer overflow-x p-3 hover:opacity-90'>
             <Image 
-                className='absolute opacity-0 lg:opacity-100 rounded-full z-50 top-10'
+                className='absolute opacity-0 lg:opacity-100 rounded-full z-1 top-10'
                 src={profile}
                 width={40}
                 height={40}
