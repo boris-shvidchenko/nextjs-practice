dd84addc2a83866c01818f7926d540de9a0018d0
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-08 16:30:05 -0700

    Update HeaderIcon.js

--- facebook-clone/components/HeaderIcon.js
+++ facebook-clone/components/HeaderIcon.js
@@ -1,6 +1,6 @@
 export default function HeaderIcon({ Icon, active }) {
     return (
-        <div className='flex items-center cursor-pointer md:px-10 sm:h-14  active:border-b-2 active:border-white group'>
+        <div className='flex items-center cursor-pointer sm:h-14 md:px-5 lg:px-10 active:border-b-2 active:border-white group'>
             <Icon className={`h-5 text-gray-500 group-hover:text-blue-500 text-center sm:h-7 mx-auto ${active && 'text-blue-500'}`}/>
         </div>
     )
