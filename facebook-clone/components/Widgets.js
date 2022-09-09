aa972298e4a6fc1f3857ce9801a0e40c9c4a9f17
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-09 13:50:45 -0700

    Update Widgets.js

--- facebook-clone/components/Widgets.js
+++ facebook-clone/components/Widgets.js
@@ -1,6 +1,40 @@
+import WidgetContacts from './WidgetContacts';
+import { SearchIcon } from '@heroicons/react/outline';
+import { DotsHorizontalIcon, VideoCameraIcon } from '@heroicons/react/solid';
+
+// temporary hardcoded contact data for project
+const contacts = [
+    {src: 'https://links.papareact.com/f0p', name: 'Jeff Bezoz'},
+    {src: 'https://links.papareact.com/kxk', name: 'Elon Musk'},
+    {src: 'https://links.papareact.com/zvy', name: 'Bill Gates'},
+    {src: 'https://links.papareact.com/snf', name: 'Mark Zuckerberg'},
+    {src: 'https://links.papareact.com/d0c', name: 'Harry Potter'},
+    {src: 'https://links.papareact.com/6gg', name: 'The Queen'},
+    {src: 'https://links.papareact.com/r57', name: 'James Bond'},
+]
+
 export default function Widgets() {
+    
+    const contactCard = contacts.map(contact => {
+        return (
+            <WidgetContacts 
+                image={contact.src}
+                name={contact.name}
+            />
+        )
+    })
+    
     return(
-        <div>
+        <div className='hidden lg:flex flex-col w-60 p-2 mt-5'>
+            <div className='flex items-center justify-between text-gray-500 mb-5'>
+                <h2 className='text-xl'>Contacts</h2>
+                <div className='flex space-x-2'>
+                    <VideoCameraIcon className='h-6 cursor-pointer' />
+                    <SearchIcon className='h-6 cursor-pointer' />
+                    <DotsHorizontalIcon className='h-6 cursor-pointer' />
+                </div>
+            </div>
+            {contactCard}
         </div>
     )
 }
