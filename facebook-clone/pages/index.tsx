3fee26bd1b38eeb0f619622d91abe766dff951ca
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-09 13:51:35 -0700

    Update index.tsx

--- facebook-clone/pages/index.tsx
+++ facebook-clone/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
-import Login from '../components/Login'
+import Login from '../components/Login';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
-import Widgets from '../components/Widgets'
+import Widgets from '../components/Widgets';
 
 export default function Home() {
   
