5463a6d57ee7078b50403a335c3989c19d5f182e
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-12 21:36:34 -0700

    Add files via upload

---
+++ instagram-clone/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'links.papareact.com',
+      'platform-lookaside.fbsbx.com',
+      'firebasestorage.googleapis.com',
+    ]
+  }
+}
---
+++ instagram-clone/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+
+      <Head>
+        <title>Instagram</title>
+        <link rel="icon" href="https://www.logo.wine/a/logo/Instagram/Instagram-Logo.wine.svg" />
+      </Head> 
+
+      <Header />
+      {/* Feed */}
+      {/* Modal */}
+
+    </div>
+  )
+}
