ff29502706b0e87ad8ff1e48e03cae25d6cb597f
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-14 16:07:01 -0700

    Update Post.js

--- instagram-clone/components/Post.js
+++ instagram-clone/components/Post.js
@@ -13,14 +13,17 @@ export default function Post({ id, username, userImg, img, caption }) {
     return(
         <div className='bg-white my-7 border rounded-sm'>
 
+            {/* Post Header */}
             <div className='flex items-center p-5'>
                 <img className='h-12 w-12 rounded-full object-contain border p-1 mr-3' src={userImg} />
                 <p className='flex-1 font font-bold'>{username}</p>
                 <DotsHorizontalIcon className='h-5 cursor-pointer' />
             </div>
 
+            {/* Post Image */}
             <img className='object-cover w-full' src={img} />
 
+            {/* Post Buttons */}
             <div className='flex justify-between px-4 pt-4'>
                 <div className='flex space-x-4'>
                     <HeartIcon className='post-buttons' />
@@ -30,15 +33,19 @@ export default function Post({ id, username, userImg, img, caption }) {
                 <BookmarkIcon className='post-buttons' />
             </div>
 
-            {/* Likes */}
-            {/* username */}
-            {/* captions */}
+            {/* Post Caption */}
+            <p className='p-5 truncate'><span className='font-bold mr-1'>{username}</span>{caption}</p>
 
-            <div>
-                {/* Smiley Face */}
-                {/* Input */}
-                {/* Post button (submission) */}
-            </div>
+            {/* Post Comments */}
+
+            {/* Post Comment Input Box */}
+            <form className='flex items-center justify-between p-4'>
+                <div className='flex items-center'>
+                    <EmojiHappyIcon className='post-buttons'/>
+                    <input className='border-none focus:ring-0 outline-none' type='text' placeholder='Add a comment...' />
+                </div>
+                <button className='font-semitbold text-blue-400'>Post</button>
+            </form>
 
         </div>
     )
