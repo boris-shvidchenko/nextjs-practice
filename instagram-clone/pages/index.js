51bca9b7f9387a14a08372bb79478a006177f4ae
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-13 21:15:16 -0700

    Update index.js

--- instagram-clone/pages/index.js
+++ instagram-clone/pages/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Head from 'next/head';
 
 import Header from '../components/Header'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-gray-50 h-screen overflow-y-scroll scrollbar-hide'>
 
       <Head>
         <title>Instagram</title>
@@ -13,7 +14,7 @@ export default function Home() {
       </Head> 
 
       <Header />
-      {/* Feed */}
+      <Feed />
       {/* Modal */}
 
     </div>
