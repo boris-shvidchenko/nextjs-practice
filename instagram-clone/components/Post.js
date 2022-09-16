3e9c6e16466e3626e84ca201cfdf96140d13f19a
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-16 16:32:18 -0700

    Update Post.js

--- instagram-clone/components/Post.js
+++ instagram-clone/components/Post.js
@@ -1,3 +1,4 @@
+import { useSession } from 'next-auth/react'
 import {
     BookmarkIcon,
     ChatIcon,
@@ -10,6 +11,9 @@ import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
 
 
 export default function Post({ id, username, userImg, img, caption }) {
+
+    const { data: session } = useSession();
+
     return(
         <div className='bg-white my-7 border rounded-sm'>
 
@@ -24,14 +28,16 @@ export default function Post({ id, username, userImg, img, caption }) {
             <img className='object-cover w-full' src={img} />
 
             {/* Post Buttons */}
-            <div className='flex justify-between px-4 pt-4'>
-                <div className='flex space-x-4'>
-                    <HeartIcon className='post-buttons' />
-                    <ChatIcon className='post-buttons' />
-                    <PaperAirplaneIcon className='post-buttons' />
-                </div>
-                <BookmarkIcon className='post-buttons' />
-            </div>
+            {session && (
+                <div className='flex justify-between px-4 pt-4'>
+                    <div className='flex space-x-4'>
+                        <HeartIcon className='post-buttons' />
+                        <ChatIcon className='post-buttons' />
+                        <PaperAirplaneIcon className='post-buttons' />
+                    </div>
+                    <BookmarkIcon className='post-buttons' />
+                </div>   
+            )}
 
             {/* Post Caption */}
             <p className='p-5 truncate'><span className='font-bold mr-1'>{username}</span>{caption}</p>
@@ -39,13 +45,15 @@ export default function Post({ id, username, userImg, img, caption }) {
             {/* Post Comments */}
 
             {/* Post Comment Input Box */}
-            <form className='flex items-center justify-between p-4'>
-                <div className='flex items-center'>
-                    <EmojiHappyIcon className='post-buttons'/>
-                    <input className='border-none focus:ring-0 outline-none' type='text' placeholder='Add a comment...' />
-                </div>
-                <button className='font-semitbold text-blue-400'>Post</button>
-            </form>
+            {session && (
+                <form className='flex items-center justify-between p-4'>
+                    <div className='flex items-center'>
+                        <EmojiHappyIcon className='post-buttons'/>
+                        <input className='border-none focus:ring-0 outline-none' type='text' placeholder='Add a comment...' />
+                    </div>
+                    <button className='font-semibold text-blue-400'>Post</button>
+                </form>
+            )}
 
         </div>
     )
