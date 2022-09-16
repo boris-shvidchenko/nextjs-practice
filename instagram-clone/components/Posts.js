bfd32bc9184e8613700a9b2d571eee649d8e2f25
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-16 16:32:31 -0700

    Update Posts.js

--- instagram-clone/components/Posts.js
+++ instagram-clone/components/Posts.js
@@ -1,43 +1,33 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { faker } from '@faker-js/faker'
+
 import Post from './Post'
 
 export default function Posts(){
 
-    // temp hardcoded data
-    const postData = [
-        {
-            id: '123',
-            username: 'ssssangha',
-            userImg:'https://links.papareact.com/3ke',
-            img: 'https://links.papareact.com/3ke', 
-            caption: 'This is DOPE!'
-        },
-        {
-            id: '23',
-            username: 'ssssangha',
-            userImg:'https://links.papareact.com/3ke',
-            img: 'https://links.papareact.com/3ke', 
-            caption: 'This is DOPE!'
-        },
-        {
-            id: '12',
-            username: 'ssssangha',
-            userImg:'https://links.papareact.com/3ke',
-            img: 'https://links.papareact.com/3ke', 
-            caption: 'This is DOPE!'
-        },
-        {
-            id: '1234',
-            username: 'ssssangha',
-            userImg:'https://links.papareact.com/3ke',
-            img: 'https://links.papareact.com/3ke', 
-            caption: 'This is DOPE!'
-        }
-    ]
+     // state for post data array
+     const[postData, setPostData] = React.useState([]);
+
+     // obtain random data from faker.js and update our state with the new array of data 
+     useEffect(() => {
+         
+         const postsFromFaker = [...Array(10)].map(() => ({
+             userId: faker.datatype.uuid(),
+             username: faker.internet.userName(),
+             caption: faker.lorem.sentence(),
+             avatar: faker.image.avatar(),
+             image: faker.image.image(),
+         }));
+         
+         setPostData(postsFromFaker);
+         
+     }, [])
 
     // map through post data array and render a new Post component for each element
     const posts = postData.map((post) => {
         return (
-            <Post key={post.id} id={post.id} username={post.username} userImg={post.userImg} img={post.img} caption={post.caption} />
+            <Post key={post.userId} id={post.userId} username={post.username} userImg={post.avatar} img={post.image} caption={post.caption} />
         )
     }) 
 
