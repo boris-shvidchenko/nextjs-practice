e46090b22727d68f0d928efbacf04cabebff2b23
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-21 16:11:04 -0700

    Update Chat.js

--- whatsapp2-build/components/Chat.js
+++ whatsapp2-build/components/Chat.js
@@ -1,13 +1,36 @@
 import styled from 'styled-components';
+import { Avatar } from '@mui/material';
+import { useRouter } from 'next/router';
 
-export default function Chat({ key, users, avatar }) {
+export default function Chat({ id, email, avatar }) {
+
+    const router = useRouter();
+
+    function enterChat() {
+        router.push(`/chat/${id}`);
+    }
+
     return(
-        <Container>
-            <p>{users}</p>
+        <Container onClick={enterChat}>
+            <UserAvatar src={avatar} />
+            <p>{email}</p>
         </Container>
     )
 }
 
 const Container = styled.div`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    padding: 15px;
+    word-break: break-word;
 
+    :hover {
+        background-color: #e9eaeb
+    }
+`;
+
+const UserAvatar = styled(Avatar)`
+    margin: 5px;
+    margin-right: 15px;
 `;
