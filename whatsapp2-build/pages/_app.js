a7ac02c4a9d89524f10cb7b0e91f2b23ccba0bd1
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-20 20:30:43 -0700

    Update _app.js

--- whatsapp2-build/pages/_app.js
+++ whatsapp2-build/pages/_app.js
@@ -1,14 +1,6 @@
 import '../styles/globals.css';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, db } from '../firebase';
-import login from './login'
 
 function MyApp({ Component, pageProps }) {
-
-  const [user] = useAuthState(auth);
-
-  if (!user) return <login />
-
   return <Component {...pageProps} />
 }
 
