53f63c5444ea16dc0774220580dd24565299b6e1
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-09-28 15:30:56 -0700

    Update index.js

--- google-clone/pages/index.js
+++ google-clone/pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Body from '../components/Body';
+import Footer from '../components/Footer';
 
 export default function Home() {
+
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen'>
       <Head>
         <title>Google</title>
+        <link rel='icon' href='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/2048px-Google_Chrome_icon_%28September_2014%29.svg.png' />
       </Head>
       <Header />
-      <Body />
+      <Body/>
+      <Footer />
     </div>
   )
 }
